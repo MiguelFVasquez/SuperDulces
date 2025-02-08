c895d07e040a79a66663c523fc8590ec25e062ac
Miguel Florez <florezvasquezjuanmiguel@gmail.com>
2025-02-08 15:18:26 -0500

    First commit

---
+++ src/main/java/co/edu/uniquindio/superdulces/SuperDulcesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.superdulces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperDulcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperDulcesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uniquindio/superdulces/SuperDulcesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.superdulces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperDulcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
